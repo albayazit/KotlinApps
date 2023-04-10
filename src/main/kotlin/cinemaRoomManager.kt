003d554dc108b92410588f8326fbd5e26eebca7d
Bayazit <bakhasanov@gmail.com>
2023-04-11 01:34:54 +0300

    cinemaRoomManager project done

--- src/main/kotlin/cinemaRoomManager.kt
+++ src/main/kotlin/cinemaRoomManager.kt
@@ -1,19 +1,20 @@
+import java.lang.Exception
+
 fun main() {
     val cinema = Cinema()
     cinema.input()
     cinema.fieldInit()
-    cinema.outputField()
-    cinema.buy()
-    cinema.math()
-    cinema.output()
-    cinema.outputField()
+    while(true) {
+        if(cinema.menu() == 1) break
+    }
 }
 
 class Cinema {
     private var rows = 0
     private var seats = 0
-    private var earn = 0
     private var price = 0
+    private var purchased = 0
+    private var earn = 0
     private var field = mutableListOf<MutableList<String>>()
 
 
@@ -24,13 +25,60 @@ class Cinema {
         seats = readln().toInt()
     }
 
-    fun buy() {
+    fun menu(): Int {
+        println("""
+            1. Show the seats
+            2. Buy a ticket
+            3. Statistics
+            0. Exit
+        """.trimIndent())
+        when (readln().toInt()) {
+            1 -> outputField()
+            2 -> buy()
+            3 -> statistics()
+            0 -> return 1
+            else -> {
+                println("Wrong input!")
+                menu()
+            }
+        }
+        return 0
+    }
+
+    private fun statistics() {
+        val percent = purchased.toDouble() / (rows * seats) * 100.0
+        val percentage = "%.2f".format(percent)
+
+        println("Number of purchased tickets: $purchased")
+        println("Percentage: $percentage%")
+        println("Current income: $$earn")
+        var income = 0
+        if(rows * seats <= 60) {
+            income = 10 * rows * seats
+        } else {
+            for(i in 0 until rows) {
+                income += if(i in 0 until rows / 2) 10 * seats
+                else 8  * seats
+            }
+        }
+        println("Total income: $$income")
+    }
+
+    private fun buy() {
         println("Enter a row number:")
         val row: Int = readln().toInt()
         println("Enter a seat number in that row:")
         val seat: Int = readln().toInt()
 
-        field[row][seat] = " B"
+        try {
+            if(field[row][seat] == " B") {
+                println("That ticket has already been purchased!")
+                buy()
+            }
+            else field[row][seat] = " B"
+        } catch (e: Exception) {
+            return println("Wrong input!")
+        }
 
         price = if(rows * seats <= 60) {
             10
@@ -38,6 +86,9 @@ class Cinema {
             if(row in 0 .. rows / 2) 10
             else 8
         }
+        earn += price
+        purchased += 1
+        println("Ticket price: $$price")
     }
 
     fun fieldInit() {
@@ -57,24 +108,7 @@ class Cinema {
         }
     }
 
-    fun math() {
-        if(rows * seats <= 60) {
-            earn = rows * seats * 10
-        }
-        else {
-            for(i in 0 .. rows) {
-                earn += if(i in 0 .. rows / 2) seats * 10
-                else seats * 8
-            }
-        }
-    }
-
-    fun output() {
-//        println("Total income:\n$$earn")
-        println("Ticket price: $$price")
-    }
-
-    fun outputField() {
+    private fun outputField() {
         println("Cinema:")
         for(i in 0 .. rows) {
             for(j in 0 .. seats) {
