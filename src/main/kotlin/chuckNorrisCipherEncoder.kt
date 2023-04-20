5d67304e1dd4fd2f107c8a0b963e1eb233cf4728
Bayazit <bakhasanov@gmail.com>
2023-04-20 04:15:24 +0300

    Creating base think

--- src/main/kotlin/chuckNorrisCipherEncoder.kt
+++ src/main/kotlin/chuckNorrisCipherEncoder.kt
@@ -1,3 +1,11 @@
 fun main() {
-
+    println("Input string:")
+    val word = readln()
+    val digits = listOf<Char>('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+    println("The result:")
+    for(ch in word) {
+        var binary = ""
+//        binary = String.format("%7S", 0,  Integer.toBinaryString(ch.toInt()))
+        println("$ch = $binary")
+    }
 }
