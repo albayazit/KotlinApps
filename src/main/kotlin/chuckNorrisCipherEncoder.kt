14573a0853a75852ec8037baca061b0f467b6b94
Bayazit <bakhasanov@gmail.com>
2023-04-21 20:35:36 +0300

    Light decompose

--- src/main/kotlin/chuckNorrisCipherEncoder.kt
+++ src/main/kotlin/chuckNorrisCipherEncoder.kt
@@ -1,11 +1,25 @@
 fun main() {
-    println("Input string:")
-    val word = readln()
-    val digits = listOf<Char>('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
-    println("The result:")
-    for(ch in word) {
-        var binary = ""
-        binary = String.format("%7S", Integer.toBinaryString(ch.toInt())).replace(' ', '0')
-        println("$ch = $binary")
+    val encoder = cipherEncoder()
+    encoder.inputData()
+    encoder.convertToBinary()
+}
+
+class cipherEncoder {
+    var binary = ""
+    var word = ""
+    var encoded = ""
+    fun inputData() {
+        println("Input string:")
+        word = readln()
+    }
+
+    fun convertToBinary() {
+        for(ch in word) {
+            binary = String.format("%7S", Integer.toBinaryString(ch.toInt())).replace(' ', '0')
+            encodeBinary(binary)
+        }
+    }
+
+    fun encodeBinary(binary: String) {
     }
 }
