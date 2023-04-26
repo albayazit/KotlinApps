3a7c0e7bb30e6b9ce9b1afb6ace2a661a17199b3
Bayazit <bakhasanov@gmail.com>
2023-04-26 13:44:05 +0300

    Encoder: predone

--- src/main/kotlin/chuckNorrisCipherEncoder.kt
+++ src/main/kotlin/chuckNorrisCipherEncoder.kt
@@ -1,55 +1,95 @@
 fun main() {
-    val cipher = cipherEncoder()
-    cipher.inputData()
-    cipher.convertToBinary()
-    cipher.decode()
-    cipher.decodeToString()
+    val cipher = CipherEncoder()
+    while (true) {
+        if(cipher.inputData() == 1) break
+    }
 }
 
-class cipherEncoder {
-    var binary = ""
-    var word = ""
-    var binaryAll = ""
-    fun inputData() {
-        println("Input encoded string:")
-        word = readln()
+class CipherEncoder {
+    private var binary = ""
+    private var word = ""
+    private var binaryAll = ""
+    fun inputData(): Int {
+        println("Please input operation (encode/decode/exit):")
+        when (val action = readln()) {
+            "encode" -> encode()
+            "decode" -> decode()
+            "exit" -> {
+                println("Bye!")
+                return 1
+            }
+            else -> {
+                println("There is no '${action}' operation")
+            }
+        }
+        return 0
     }
 
-    fun convertToBinary() {
+    private fun encode() {
+        println("Input string:")
+        word = readln()
+        convertToBinary()
+        println("Encoded string:")
+        code()
+    }
+
+    private fun convertToBinary() {
+        binary = ""
         for(ch in word) {
             binary += String.format("%7S", Integer.toBinaryString(ch.toInt())).replace(' ', '0')
         }
     }
 
-    fun code() {
+    private fun code() {
         var count = 0
+        var result = ""
         while(count != binary.length) {
             if(binary[count] == '1') {
-                print("0 ")
+                result += "0 "
                 for(i in count until binary.length) {
                     if(binary[i] == '1') {
-                        print('0')
+                        result += "0"
                         count++
                     } else break
                 }
-                print(' ')
             } else{
-                print("00 ")
+                result += "00 "
                 for(i in count until binary.length) {
                     if(binary[i] == '0') {
-                        print('0')
+                        result += "0"
                         count++
                     } else break
                 }
-                print(' ')
             }
+            result += ' '
+        }
+        println(result)
+    }
+
+    private fun decode() {
+        println("Input encoded string:")
+        word = readln()
+        for(i in word) {
+            if(i != '0' && i != ' '){
+                println("Encoded string is not valid.")
+                return
+            }
+        }
+        if(codeToBinary() == 1) {
+            println("Encoded string is not valid.")
+            return
+        }
+        if(decodeToString() == 1) {
+            println("Encoded string is not valid.")
+            return
         }
     }
 
-    fun decode() {
+    private fun codeToBinary(): Int {
         var count = 0
+        binaryAll = ""
         while(count < word.length) {
-            if (word[count] == '0' && word[count + 1] == '0') {
+            if (word[count] == '0' && word[count + 1] == '0' && word[count + 2] == ' ') {
                 count += 3
                 for (i in count until word.length) {
                     count++
@@ -57,7 +97,7 @@ class cipherEncoder {
                         binaryAll += '0'
                     } else break
                 }
-            } else {
+            } else if (word[count] == '0' && word[count + 1] == ' ') {
                 count += 2
                 for (i in count until word.length) {
                     count++
@@ -65,15 +105,22 @@ class cipherEncoder {
                         binaryAll += '1'
                     } else break
                 }
-            }
+            } else return 1
         }
+        return 0
     }
 
-    fun decodeToString() {
-        println("The result:")
+    private fun decodeToString(): Int {
+        var result = ""
         val chunkedBinary = binaryAll.chunked(7)
-        for(i in chunkedBinary.indices) {
-            print(Integer.parseInt(chunkedBinary[i], 2).toChar())
+        for(i in chunkedBinary) {
+            if(i.length != 7) return 1
         }
+        println("Decoded string:")
+        for(i in chunkedBinary.indices) {
+            result += Integer.parseInt(chunkedBinary[i], 2).toChar()
+        }
+        println(result)
+        return  0
     }
 }
