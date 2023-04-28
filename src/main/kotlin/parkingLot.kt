9d7578b3bafd40dcf63aa2c58631ed9ed03e2f7b
Bayazit <bakhasanov@gmail.com>
2023-04-28 04:22:16 +0300

    Replace enum class into default class and realize create/status functions

--- src/main/kotlin/parkingLot.kt
+++ src/main/kotlin/parkingLot.kt
@@ -1,5 +1,3 @@
-import javax.print.DocFlavor.STRING
-
 fun main() {
     val park = Parking()
     while(true) {
@@ -8,10 +6,14 @@ fun main() {
 }
 
 class Parking {
-    lateinit var data: List<String>
+    private lateinit var data: List<String>
+    private val db = DATABASE().database
+
     fun input(): Int {
         data = readln().split(' ')
         when(data[0]) {
+            "create" -> create()
+            "status" -> status()
             "park" -> park()
             "leave" -> leave()
             "exit" -> return 1
@@ -19,37 +21,69 @@ class Parking {
         return 0
     }
 
-    fun park() {
+    private fun create() {
+        db.clear()
+        val count = data[1].toInt()
+        for(i in 1..count) {
+            db.add(mutableListOf("$i", "NULL", "NULL", "FREE"))
+        }
+        println("Created a parking lot with $count spots.")
+    }
+
+    private fun status() {
+        if(dataCheck() == 1) return
+        var emptyCheck = true
+        for(i in db.indices) {
+            if(db[i][2] != "NULL") {
+                println("${db[i][0]} ${db[i][1]} ${db[i][2]}")
+                emptyCheck = false
+            }
+        }
+        if(emptyCheck) println("Parking lot is empty.")
+    }
+
+    private fun park() {
+        if(dataCheck() == 1) return
         val color = data[2]
-        for(i in DATABASE.values()) {
-            if(i.status == "FREE") {
-                println("$color car parked in spot ${i.ordinal + 1}.")
-                i.status = "BUSY"
+        for(i in db.indices) {
+            if(db[i][3] == "FREE") {
+                println("$color car parked in spot ${i + 1}.")
+                db[i][3] = "BUSY"
+                db[i][1] = data[1]
+                db[i][2] = data[2]
                 return
             }
         }
         println("Sorry, the parking lot is full.")
     }
 
-    fun leave() {
+    private fun leave() {
+        if(dataCheck() == 1) return
         val spot = data[1].toInt()
-        for(i in DATABASE.values()) {
-            if(i.ordinal == spot - 1) {
-                if(i.status == "FREE") {
+        for(i in db.indices) {
+            if(i == spot - 1) {
+                if(db[i][3] == "FREE") {
                     println("There is no car in spot ${spot}.")
-                }
-                else {
-                    println("Spot ${spot} is free.")
-                    i.status = "FREE"
+                } else {
+                    println("Spot $spot is free.")
+                    db[i][3] = "FREE"
+                    db[i][2] = "NULL"
+                    db[i][1] = "NULL"
                 }
                 break
             }
         }
     }
 
+    private fun dataCheck(): Int {
+        if(db.isEmpty()) {
+            println("Sorry, a parking lot has not been created.")
+            return 1
+        }
+        return 0
+    }
 }
 
-enum class DATABASE(var number: String = "NULL", var status: String = "FREE") {
-    SPOT1(), SPOT2(), SPOT3(), SPOT4(), SPOT5(), SPOT6(), SPOT7(), SPOT8(), SPOT9(), SPOT10(),
-    SPOT11(), SPOT12(), SPOT13(), SPOT14(), SPOT15(), SPOT16(), SPOT17(), SPOT18(), SPOT19(), SPOT20()
+class DATABASE {
+    var database = mutableListOf<MutableList<String>>()
 }
