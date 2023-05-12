10a41347dd1f913e9f7c039a6768181057de2bf3
Bayazit <bakhasanov@gmail.com>
2023-05-12 06:44:15 +0300

    Added input function

--- src/main/kotlin/indigoCardGame.kt
+++ src/main/kotlin/indigoCardGame.kt
@@ -1,5 +1,23 @@
 fun main() {
-    println("A 2 3 4 5 6 7 8 9 10 J Q K")
-    println("♦ ♥ ♠ ♣")
-    println("A♠ 2♠ 3♠ 4♠ 5♠ 6♠ 7♠ 8♠ 9♠ 10♠ J♠ Q♠ K♠ A♥ 2♥ 3♥ 4♥ 5♥ 6♥ 7♥ 8♥ 9♥ 10♥ J♥ Q♥ K♥ A♦ 2♦ 3♦ 4♦ 5♦ 6♦ 7♦ 8♦ 9♦ 10♦ J♦ Q♦ K♦ A♣ 2♣ 3♣ 4♣ 5♣ 6♣ 7♣ 8♣ 9♣ 10♣ J♣ Q♣ K♣")
+    val game = Indigo()
+    while (true) {
+        if(game.input() == 1) return
+    }
+}
+
+class Indigo {
+    fun input(): Int {
+        println("Choose an action (reset, shuffle, get, exit):")
+        when (readln()) {
+            "reset" -> reset()
+            "shuffle" -> shuffle()
+            "get" -> get()
+            "exit" -> return 1
+        }
+        return 0
+    }
+
+    fun reset() {}
+    fun shuffle() {}
+    fun get() {}
 }
