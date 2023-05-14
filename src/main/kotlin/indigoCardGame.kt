5fc46d18c62fa2024098d0239a5a9d6f5f331fb7
Bayazit <bakhasanov@gmail.com>
2023-05-14 23:43:07 +0300

    Show initial cards and decompositon IndigoGame class

--- src/main/kotlin/indigoCardGame.kt
+++ src/main/kotlin/indigoCardGame.kt
@@ -1,45 +1,43 @@
 fun main() {
     val game = IndigoGame()
-    while (true) {
-        if(game.input() == 1) break
+    println("Indigo Card Game")
+    game.intro()
+}
+
+class Card {
+    var cards = mutableListOf("A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣")
+    var cardsInTable = mutableListOf<String>()
+
+    fun resetCards() {
+        cards = mutableListOf("A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣")
+        println("Card deck is reset.")
+    }
+    fun shuffleCards() {
+        cards.shuffle()
+//        println("Card deck is shuffled.")
+    }
+    fun getCards() {
+        cardsInTable.add(cards[cards.lastIndex - 3])
+        cardsInTable.add(cards[cards.lastIndex - 2])
+        cardsInTable.add(cards[cards.lastIndex - 1])
+        cardsInTable.add(cards[cards.lastIndex])
     }
 }
 
 class IndigoGame {
-    private var cards = mutableListOf("A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣")
+    val game = Card()
 
-    fun input(): Int {
-        println("Choose an action (reset, shuffle, get, exit):")
-        when (readln()) {
-            "reset" -> resetCards()
-            "shuffle" -> shuffleCards()
-            "get" -> getCards()
-            "exit" -> {
-                println("Bye")
-                return 1
-            }
-            else -> println("Wrong action.")
-        }
-        return 0
+    fun intro() {
+        game.shuffleCards()
+        println("Play first?")
+        val action = readln()
+        if (action.uppercase() == "YES") initialCards()
+        else if (action.uppercase() == "NO") initialCards()
+        else intro()
     }
 
-    private fun resetCards() {
-        cards = mutableListOf("A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣")
-        println("Card deck is reset.")
-    }
-    private fun shuffleCards() {
-        cards.shuffle()
-        println("Card deck is shuffled.")
-    }
-    private fun getCards() {
-        println("Number of cards:")
-        val number = readln().toIntOrNull() ?: return println("Invalid number of cards.")
-        if(number !in 1..52) return println("Invalid number of cards.")
-        if(number > cards.size) return println("The remaining cards are insufficient to meet the request.")
-        repeat (number) {
-            print("${cards[cards.lastIndex]} ")
-            cards.removeAt(cards.lastIndex)
-        }
-        println()
+    private fun initialCards() {
+        game.getCards()
+        println(game.cardsInTable.joinToString().replace(", ", " "))
     }
 }
