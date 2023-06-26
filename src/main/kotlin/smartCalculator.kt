e5b16bc67ba8ab81199b49ca95e9f51ccd84ba8f
albayazit <bakhasanov@gmail.com>
2023-06-26 21:15:56 +0300

    Add a loop

--- src/main/kotlin/smartCalculator.kt
+++ src/main/kotlin/smartCalculator.kt
@@ -6,11 +6,21 @@ fun main() {
 class Calculator {
     val operation = Operation()
     fun start() {
-        val numbers = input().split(" ")
-        val a = numbers[0].toInt()
-        val b = numbers[1].toInt()
-        val result = operation.add(a, b)
-        println(result)
+        while (true) {
+            val numbers = input().split(" ")
+            if (numbers[0] == "/exit") {
+                println("Bye!")
+                break
+            }
+            else if (numbers[0] == "") continue
+            else if (numbers.size == 1) println(numbers[0])
+            else {
+                val a = numbers[0].toIntOrNull() ?: 0
+                val b = numbers[1].toIntOrNull() ?: 0
+                val result = operation.add(a, b)
+                println(result)
+            }
+        }
     }
 
     fun input(): String {
